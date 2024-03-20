d7e2ad7c4d52c2597ed418908a6efdf42a3f6668
Lotses <Lotses@mail.ru>
2024-03-20 20:29:47 +0300

    main method created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] initialArray = { "apple", "1234", "dot", "babushka", "_test_text" };
+        string[] newArray = new string[initialArray.Length];
+        int newIndex = 0;
+
+        for (int i = 0; i < initialArray.Length; i++)
+        {
+            if (initialArray[i].Length <= 3)
+            {
+                newArray[newIndex] = initialArray[i];
+                newIndex++;
+            }
+        }
+
+        Array.Resize(ref newArray, newIndex);
+
+        
+    }
+}
